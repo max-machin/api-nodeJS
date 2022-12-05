3bc540541d27dca6e0ee7d23881dac8c100803ce
Machin Max <machin.max@laplateforme.io>
2022-12-05 14:28:16 +0100

    switch branch

--- Database/Database.js
+++
@@ -1,9 +0,0 @@
-const mysql = require('mysql')
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'api-nodejs'
-})
-
-return connection.connect() 
